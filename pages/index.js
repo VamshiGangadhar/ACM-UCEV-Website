352c796e510a0f023970fbc66eb7c68c9593b609
VamshiGangadhar <80575671+VamshiGangadhar@users.noreply.github.com>
2023-01-04 23:57:40 +0530

    Updated posts and events codes in main page

--- pages/index.js
+++ pages/index.js
@@ -23,41 +23,113 @@ export const getStaticProps = async () => {
             url
           }
         }
-        posts(sort: "created_at:desc", limit: 5) {
-          id
-          Title
-          Slug
-          created_at
-          updated_at
-          Description
-          authors {
+        posts(sort: "createdAt:desc") {
+    data {
+      id
+    }
+    data {
+      attributes {
+        Title
+      }
+    }
+    data {
+      attributes {
+        Slug
+      }
+    }
+    data {
+      attributes {
+        createdAt
+      }
+    }
+    data {
+      attributes {
+        Description
+      }
+    }
+    data {
+      attributes {
+        authors {
+          data {
             id
-            Author_name
-            Author_image {
+          }
+          data {
+            attributes {
+              Author_name
+            }
+          }
+          data {
+            attributes {
+              Author_image {
+                data {
+                  attributes {
+                    url
+                  }
+                }
+              }
+            }
+          }
+          data {
+            attributes {
+              Author_bio
+            }
+          }
+        }
+      }
+    }
+    data {
+      attributes {
+        tags {
+          data {
+            id
+          }
+          data {
+            attributes {
+              Tag_name
+            }
+          }
+        }
+      }
+    }
+    data {
+      attributes {
+        Cover_image {
+          data {
+            attributes {
               url
             }
           }
-          tags {
-            id
-            Tag_name
-          }
-          Cover_image {
-            url
-          }
-          Mini_description
         }
+      }
+    }
+    data {
+      attributes {
+        Mini_description
+      }
+    }
+  }
         events(sort: "Start_time:asc") {
-          id
-          created_at
-          Event_name
-          Start_time
-          End_time
-          Mini_description
-          event_tags {
-            Tag_name
-          }
-          Slug
-        }
+    data {
+      attributes {
+        Event_name
+      }
+    }
+    data {
+      attributes {
+        Start_time
+      }
+    }
+    data {
+      attributes {
+        Slug
+      }
+    }
+    data {
+      attributes {
+        End_time
+      }
+    }
+  }
       }
     `,
   });
